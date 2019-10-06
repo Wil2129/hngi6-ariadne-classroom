939de1c90125184c368b82bb74dd74063de7b0c7
Teddy BAHA <michael_takeshi@yahoo.com>
2019-10-06 12:34:03 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,10 @@
 <?php
 declare(strict_types=1);
+
+if (! isset($_COOKIE["CurrentUser"]) and ! isset($_SESSION['current_user'])) {
+    header('Location: signin.php');
+}
+
 require_once "backend/server.php";
 require_once "frontend/home.html";
 ?>
