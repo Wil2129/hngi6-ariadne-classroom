4ab497b3943e045811a7ce12508fdf3cc14325e0
Teddy BAHA <michael_takeshi@yahoo.com>
2019-10-06 12:30:01 +0100

    added home.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 declare(strict_types=1);
+
+if (isset($_COOKIE["CurrentUser"]) or isset($_SESSION['current_user'])) {
+    header('Location: home.php');
+}
+
 require_once "frontend/index.html";
 ?>
